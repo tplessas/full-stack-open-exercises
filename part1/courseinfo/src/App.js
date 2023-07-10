f37f1c3121eab99b2a46bbd511345b32c664c370
tplessas <services@tplessas.eu>
2023-07-11 00:28:07 +0300

    part1(ex1.1): add courseinfo project

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>;
+};
+
+const Content = ({
+  partName1,
+  partName2,
+  partName3,
+  exercisesCount1,
+  exercisesCount2,
+  exercisesCount3,
+}) => {
+  return (
+    <>
+      <p>
+        {partName1} {exercisesCount1}
+      </p>
+      <p>
+        {partName2} {exercisesCount2}
+      </p>
+      <p>
+        {partName3} {exercisesCount3}
+      </p>
+    </>
+  );
+};
+
+const Total = ({ exercisesCount1, exercisesCount2, exercisesCount3 }) => {
+  return (
+    <p>
+      Number of exercises {exercisesCount1 + exercisesCount2 + exercisesCount3}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header courseName={course} />
+      <Content
+        partName1={part1}
+        exercisesCount1={exercises1}
+        partName2={part2}
+        exercisesCount2={exercises2}
+        partName3={part3}
+        exercisesCount3={exercises3}
+      />
+      <Total
+        exercisesCount1={exercises1}
+        exercisesCount2={exercises2}
+        exercisesCount3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
