e0f8e1866d0694a3064ec35da36e9498fcb6a0b4
tplessas <services@tplessas.eu>
2023-07-11 14:55:35 +0300

    part1(ex1.4): refactor App and dependent components to use object array

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,12 +2,12 @@ const Header = ({ courseName }) => {
   return <h1>{courseName}</h1>;
 };
 
-const Content = ({ part1, part2, part3 }) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part {...part1} />
-      <Part {...part2} />
-      <Part {...part3} />
+      <Part {...parts[0]} />
+      <Part {...parts[1]} />
+      <Part {...parts[2]} />
     </>
   );
 };
@@ -20,38 +20,37 @@ const Part = ({ name, exercises }) => {
   );
 };
 
-const Total = ({ exercisesCount1, exercisesCount2, exercisesCount3 }) => {
+const Total = ({ parts }) => {
   return (
     <p>
-      Number of exercises {exercisesCount1 + exercisesCount2 + exercisesCount3}
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
     </p>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header courseName={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total
-        exercisesCount1={part1.exercises}
-        exercisesCount2={part2.exercises}
-        exercisesCount3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
